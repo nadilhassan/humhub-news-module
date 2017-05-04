e7ead78465bc6974baeebf63e83934395642eeef
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-05-03 22:56:10 -0400

    Update form.php

--- widgets/views/form.php
+++ widgets/views/form.php
@@ -6,57 +6,51 @@ use yii\helpers\Url;
 humhub\modules\news\Assets::register($this);
 ?>
 
-
 <br>
 
 <div id="txtEditorNews" class="row">
-
 
     <div class="col-md-12">
 
         <div id="">
             <div class="arrow"></div>
-
-
             <div class="col-md-12 ">
 
                 <input type="hidden" name="_csrf" value="<?= Yii::$app->request->getCsrfToken() ?>"/>
                 <div class="form-group">
-                    <?php
-                    echo Html::textInput('title', '', array('id' => 'contentForm_title', 'class' => 'form-control contentForm', 'placeholder' => 'Title'))
-
-                    ?></div>
-                <!-- <iframe style="height: 350px; width: 100%;border: 3px solid #9d9d9d;" name="text"
-                    class="form-control autosize contentForm"     id="contentForm_text"></iframe>-->
+                    <?= Html::textInput('title', '', ['id' => 'contentForm_title', 'class' => 'form-control contentForm', 'placeholder' => 'Title']); ?>
+                   </div>
+                /* <iframe style="height: 350px; width: 100%;border: 3px solid #9d9d9d;" name="text"
+                    class="form-control autosize contentForm" id="contentForm_text"></iframe> */
                 <div class="form-group">
 
-                    <?php echo Html::textArea("text", '', array('id' => 'contentForm_text', 'class' => 'form-control autosize contentForm', 'rows' => '14', "tabindex" => "1", 'placeholder' => 'Write something...')); ?>
-                    <?php echo \humhub\widgets\MarkdownEditor::widget(array('fieldId' => 'contentForm_text')); ?>
-                    <!--                        --><?php //echo \humhub\widgets\RichTextEditor::widget(array('id' => 'contentForm_text')); ?>
+                    <?= Html::textArea("text", '', ['id' => 'contentForm_text', 'class' => 'form-control autosize contentForm', 'rows' => '14', "tabindex" => "1", 'placeholder' => 'Write something...']); ?>
+                    <?= \humhub\widgets\MarkdownEditor::widget(['fieldId' => 'contentForm_text']); ?>
+                    <!--                        --><?php //echo \humhub\widgets\RichTextEditor::widget(['id' => 'contentForm_text']); ?>
                 </div>
+
                 <div class="form-group">
                     <a class="colorPrimary" role="button" data-toggle="collapse" href="#authorCollapse"
                        aria-expanded="false" aria-controls="collapseExample">
                         Edit Author
                     </a>
+
                     <div class="collapse" style="margin-top: 15px;" id="authorCollapse">
-                        <div class="">
-                            <?php echo Html::textInput('changeAuthor', '', array('id' => 'changeAuthor', 'placeholder' => '')); ?>
+                        <div class="contentForm_options">
+                            <?= Html::textInput('changeAuthor', '', ['id' => 'changeAuthor', 'placeholder' => 'Assign user(s) for this news article.']); ?>
 
-                            <?php
-
-                            echo humhub\modules\user\widgets\UserPicker::widget(array(
+                            <?=
+                             humhub\modules\user\widgets\UserPicker::widget([
                                 'inputId' => 'changeAuthor',
-                                'userSearchUrl' => $container->createUrl('/space/membership/search', array('keyword' => '-keywordPlaceholder-')),
+                                'userSearchUrl' => $container->createUrl('/space/membership/search', ['keyword' => '-keywordPlaceholder-']),
                                 'maxUsers' => 10,
-                                'placeholderText' => 'Assign An Author',
-                            ));
+                                'placeholderText' => 'Assign author to article.',
+                            ]);
                             ?>
                         </div>
                     </div>
-
-
                 </div>
+
                 <div class="form-group">
                     <a class="colorPrimary" role="button" data-toggle="collapse" href="#collapseNewsLay"
                        aria-expanded="false" aria-controls="collapseExample">
@@ -67,13 +61,11 @@ humhub\modules\news\Assets::register($this);
                         <div class="">
                             <?php
                             foreach ($layouts as $lay):
-
                                 ?>
                                 <label class="change_layout">
                                     <p class="text-center"><?= strtoupper($lay->name) ?></p>
                                     <input id="lay<?= $lay->id; ?>" type="radio" name="lay"
                                            value="<?= $lay->id; ?>"/>
-
 
                                     <?php
                                     if ($lay->name == "loud") {
@@ -85,41 +77,31 @@ humhub\modules\news\Assets::register($this);
                                     } else if ($lay->name == "default") {
                                         $fileUrl = Yii::$app->getModule('news')->getAssetsUrl() . '/layout_4.jpg';
                                         echo '<img style="width: 100px;" src="' . $fileUrl . '">';
-
                                     } else if ($lay->name == "loud story") {
                                         $fileUrl = Yii::$app->getModule('news')->getAssetsUrl() . '/layout_5.jpg';
                                         echo '<img style="width: 100px;" src="' . $fileUrl . '">';
-
-
                                     }
                                     ?>
-
                                 </label>
-                                <?php
-                            endforeach;
-                            ?>
+
+                                <?php endforeach; ?>
                         </div>
                     </div>
                 </div>
 
-
                 <!-- --><? /*= Html::endForm() */ ?>
             </div>
-
-
         </div>
     </div>
-
 </div>
+
 <br>
-<?php //echo Html::textArea("text", '', array('id' => 'contentForm_text', 'class' => 'form-control autosize contentForm', 'rows' => '14', "tabindex" => "1", 'placeholder' => 'Write something...'));
 
+<? //= Html::textArea("text", '', ['id' => 'contentForm_text', 'class' => 'form-control autosize contentForm', 'rows' => '14', "tabindex" => "1", 'placeholder' => 'Write something...']); ?>
 
-?>
-<!--<script type="text/javascript">
+/*<script type="text/javascript">
     $(function () {
         $('#featuredImageFile').fileupload({
-
             dataType: 'json',
             progressall: function (e, data) {
                 var progress = parseInt(data.loaded / data.total * 100, 10);
@@ -134,8 +116,7 @@ humhub\modules\news\Assets::register($this);
                     $('<p/>').text(file.name).appendTo(document.body);
                 });
             },
-
         });
     });
 </script>
--->
+*/
