01cc2228c1cbd5843b5a0264b2795191b4889c9d
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-05-06 22:29:36 -0400

    Update WallEntry.php

--- widgets/WallEntry.php
+++ widgets/WallEntry.php
@@ -9,22 +9,35 @@
 
 namespace humhub\modules\news\widgets;
 
+use Yii;
 use humhub\modules\news\models\NewsLayouts;
 use humhub\modules\news\models\UsersNewsLayout;
-use Yii;
 
 class WallEntry extends \humhub\modules\content\widgets\WallEntry
 {
-
+    public $news;
     public $editRoute = '/news/news/edit';
-    public $showFiles = 'false';
+    public $showFiles = false;
 
     public function run()
     {
-        return $this->render('wallentry', [
-            'news' => $this->contentObject,
-            'user' => $this->contentObject->content->user,
-            'contentContainer' => $this->contentObject->content->container,
-            ]);
+        $user = $this->contentObject->content->user;
+
+        $currentUserAssigned = false;
+
+        // Check if current user is assigned to this article
+        foreach ($this->contentObject->assignedUsers as $au) {
+            if ($au->id == Yii::$app->user->id) {
+                $currentUserAssigned = true;
+                break;
+            }
         }
+        return $this->render('entry', array(
+                    'task' => $this->contentObject,
+                    'user' => $user,
+                    'contentContainer' => $this->contentObject->content->container,
+                    'assignedUsers' => $this->contentObject->assignedUsers,
+                    'currentUserAssigned' => $currentUserAssigned
+        ));
     }
+}
