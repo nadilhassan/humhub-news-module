042b232b52d62596ef891edd14dcde4295bf691e
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-10-08 11:14:04 +0900

    Update CreateNews.php

--- permissions/CreateNews.php
+++ permissions/CreateNews.php
@@ -1,48 +1,27 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: USER
- * Date: 6/2/2016
- * Time: 9:40 AM
- */
-
 namespace humhub\modules\news\permissions;
 
+use humhub\libs\BasePermission;
 use humhub\modules\space\models\Space;
 
-class CreateNews extends \humhub\libs\BasePermission
+class CreateNews extends BasePermission
 {
-
-    /**
-     * @inheritdoc
-     */
     public $defaultAllowedGroups = [
         Space::USERGROUP_OWNER,
         Space::USERGROUP_ADMIN,
         Space::USERGROUP_MODERATOR,
-        Space::USERGROUP_MEMBER,
+        Space::USERGROUP_MEMBERS,
+        Space::USERGROUP_USERS,
     ];
 
-    /**
-     * @inheritdoc
-     */
     protected $fixedGroups = [
-        Space::USERGROUP_MEMBER
+        Space::USERGROUP_USERS
     ];
 
-    /**
-     * @inheritdoc
-     */
-    protected $title = 'Create News';
+    protected $title = "Create News";
 
-    /**
-     * @inheritdoc
-     */
-    protected $description = 'Allows the user to share their story';
+    protected $description = "Allows the user to share their story";
 
-    /**
-     * @inheritdoc
-     */
     protected $moduleId = 'news';
 }
